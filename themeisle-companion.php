3a46c4bec4e59d058ebb507061039bb05ff9dd2a
David Odehnal <info@davidodehnal.cz>
2017-09-23 23:39:38 +0200

    Update themeisle-companion.php

--- themeisle-companion.php
+++ themeisle-companion.php
@@ -37,3 +37,69 @@ function themeisle_companion_loader() {
 }
 
 add_action( 'after_setup_theme', 'themeisle_companion_loader', 0 );
+
+function scout_companion_check_for_updates( $transient ) {
+
+	if ( empty( $transient->checked ) ) {
+		return $transient;
+	}
+
+	$pluginName = '';
+
+	foreach ( $transient->checked as $plugin => $version ) {
+		if ( strpos( $plugin, 'dsw-skaut-svs-companion' ) !== false ) {
+			$pluginName = $plugin;
+		}
+	}
+
+	if ( $pluginName === '' ) {
+		return $transient;
+	}
+
+	$raw = wp_remote_get( 'https://api.github.com/repos/skaut/dsw-skaut-svs-companion/releases/latest', [ 'user-agent' => 'skaut' ] );
+	if ( is_wp_error( $raw ) || wp_remote_retrieve_response_code( $raw ) !== 200 ) {
+		return $transient;
+	}
+
+	$actual = json_decode( $raw['body'] );
+	if ( $actual === null ) {
+		return $transient;
+	}
+
+	if ( preg_match( '~\d+\.\d+~', $actual->tag_name ) === 1 ) {
+		$package = $actual->zipball_url;
+
+		if ( strpos( $actual->tag_name, 'v' ) === 0 ) {
+			$version = substr( $actual->tag_name, 1 );
+		} else {
+			$version = $actual->tag_name;
+		}
+
+		if ( $package !== null && version_compare( $transient->checked[ $pluginName ], $version, '<' ) ) {
+			$transient->response[ $pluginName ] = [
+				'new_version' => $version,
+				'url'         => $actual->html_url,
+				'package'     => $package
+			];
+		}
+	}
+
+	return $transient;
+}
+
+add_filter( 'pre_set_site_transient_update_plugins', 'scout_companion_check_for_updates' );
+
+add_filter( 'upgrader_source_selection', function ( $source ) {
+	if ( strpos( $source, 'dsw-skaut-svs-companion' ) === false ) {
+		return $source;
+	}
+
+	preg_match( "~([^\/]+)\/$~", $source, $result );
+	if ( ! isset( $result[1] ) ) {
+		return $source;
+	}
+	$newFolderName = str_replace( $result[1], 'dsw-skaut-svs-companion', $source );
+	rename( $source, $newFolderName );
+
+	return $newFolderName;
+}, 20, 1 );
